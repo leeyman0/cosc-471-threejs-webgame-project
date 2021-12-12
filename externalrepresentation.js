8b7e1690ce9386440471b49afd13c75cfda0aa62
Stann Leey <stannleey0@gmail.com>
2021-12-11 22:28:52 -0500

    Got the UI working in proper resolution

--- externalrepresentation.js
+++ externalrepresentation.js
@@ -1,6 +1,14 @@
 // Put the code that generates graphics here
 function updateScene(state, tapestry) {
     tapestry.renderer.render(tapestry.scene, tapestry.camera);
+
+    tapestry.textContext.font = "15pt Helvetica";
+    tapestry.textContext.fillStyle = healthGradient;
+    tapestry.textContext.fillText("Health", 20, 20);
+    tapestry.textContext.fillRect(20, 40,
+				  state.player.health
+				  / PLAYER_BASE_HEALTH
+				  * FULL_HEALTH_BAR_WIDTH, 10);
     return;
 }
 
